96746622e91bfd08c5d1053ad487ae3a0f6f036e
raddevus <1radeutsch1@gmail.com>
2026-03-29 22:24:04 -0400

    Added Clipboard helper class from my Avalonia 11 projects

---
+++ congruent/Models/Clipboard.cs
@@ -0,0 +1,26 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.VisualTree;
+using Avalonia.Input.Platform;
+using Avalonia.Controls.ApplicationLifetimes;
+
+namespace AppHelpers; 
+public class Clipboard {
+    public static IClipboard Get() {
+
+        //Desktop
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime { MainWindow: { } window }) {
+            return window.Clipboard!;
+
+        }
+        //Android (and iOS?)
+        else if (Application.Current?.ApplicationLifetime is ISingleViewApplicationLifetime { MainView: { } mainView }) {
+            var visualRoot = mainView.GetVisualRoot();
+            if (visualRoot is TopLevel topLevel) {
+                return topLevel.Clipboard!;
+            }
+        }
+
+        return null!;
+    }
+}
