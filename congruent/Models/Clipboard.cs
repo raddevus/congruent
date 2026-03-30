e2aafc662cfe8eea2844f15e1ce864c28ca18743
raddevus <1radeutsch1@gmail.com>
2026-03-29 22:26:38 -0400

    Updated in an attempt to get class working in Av 12 prerelease

--- congruent/Models/Clipboard.cs
+++ congruent/Models/Clipboard.cs
@@ -4,23 +4,33 @@ using Avalonia.VisualTree;
 using Avalonia.Input.Platform;
 using Avalonia.Controls.ApplicationLifetimes;
 
-namespace AppHelpers; 
-public class Clipboard {
-    public static IClipboard Get() {
+namespace AppHelpers;
 
-        //Desktop
-        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime { MainWindow: { } window }) {
+public class Clipboard
+{
+    public static IClipboard Get()
+    {
+        // Desktop
+        if (Application.Current?.ApplicationLifetime
+            is IClassicDesktopStyleApplicationLifetime { MainWindow: { } window })
+        {
             return window.Clipboard!;
-
         }
-        //Android (and iOS?)
-        else if (Application.Current?.ApplicationLifetime is ISingleViewApplicationLifetime { MainView: { } mainView }) {
-            var visualRoot = mainView.GetVisualRoot();
-            if (visualRoot is TopLevel topLevel) {
-                return topLevel.Clipboard!;
+
+        // Android / iOS / Mobile
+        if (Application.Current?.ApplicationLifetime
+            is ISingleViewApplicationLifetime { MainView: { } mainView })
+        {
+            // Avalonia 12: MainView is Control, but GetVisualRoot() is on IVisual
+            if (mainView is IVisual visual)
+            {
+                var root = visual.GetVisualRoot();
+                if (root is TopLevel topLevel)
+                    return topLevel.Clipboard!;
             }
         }
 
         return null!;
     }
 }
+
