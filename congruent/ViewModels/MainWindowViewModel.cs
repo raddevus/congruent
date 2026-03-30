8d5cac8847fdc0213f2485cdd4578063e6d0f425
raddevus <1radeutsch1@gmail.com>
2026-03-30 17:08:50 -0400

    Added observable all bookmarks to hold the list

--- congruent/ViewModels/MainWindowViewModel.cs
+++ congruent/ViewModels/MainWindowViewModel.cs
@@ -6,5 +6,6 @@ namespace congruent.ViewModels;
 public partial class MainWindowViewModel : ViewModelBase
 {
    public ObservableCollection<WebLink> AllLinks{get;set;} = new();
+   public ObservableCollection<Bookmark> AllBookmarks{get;set;} = new();
 
 }
