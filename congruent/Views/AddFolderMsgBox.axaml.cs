dbaf9abe9a298468c99059a29ec5f2c1ff901981
raddevus <1radeutsch1@gmail.com>
2026-04-04 16:57:21 -0400

    Added new msg box for add folder

---
+++ congruent/Views/AddFolderMsgBox.axaml.cs
@@ -0,0 +1,38 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using congruent.Models;
+
+namespace congruent.Views;
+
+public partial class AddFolderMsgBox : Window
+{
+   public string FolderName {get;set;}
+
+   public AddFolderMsgBox(): this("default message") {
+   }
+    public AddFolderMsgBox(string message)
+    {
+        InitializeComponent();
+        MessageText.Text = message;
+        // Sets Focus to the SiteKey text box
+        this.Opened += (_, __) => {LinkTitleTB.Focus(); };
+    }
+
+    private void Ok_Click(object? sender, RoutedEventArgs e)
+    {
+       // replace all white-space anywhere in string
+       LinkTitle = LinkTitleTB?.Text?.Replace(" ", "") ?? string.Empty;
+       LinkUrl = LinkUrlTB?.Text?.Replace(" ", "") ?? string.Empty;
+       if (string.IsNullOrEmpty(LinkTitle)){
+          MessageText.Text = "The string must be at least 1 character and not contain any spaces.";
+          LinkTitleTB.Focus();
+          return;
+       }
+       Close(true);
+    }
+
+    private void Cancel_Click(object? sender, RoutedEventArgs e)
+    {
+        Close(false);
+    }
+}
