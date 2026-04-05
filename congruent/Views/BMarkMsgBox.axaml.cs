790d6039bcfe42f0a99df7c3c17a18c5a97a2004
raddevus <1radeutsch1@gmail.com>
2026-04-05 10:43:12 -0400

    Switched order of validation checks to mimic the form layout

--- congruent/Views/BMarkMsgBox.axaml.cs
+++ congruent/Views/BMarkMsgBox.axaml.cs
@@ -23,14 +23,14 @@ public partial class BMarkMsgBox : Window
        // replace all white-space anywhere in string
        LinkTitle = LinkTitleTB?.Text?.Replace(" ", "") ?? string.Empty;
        LinkUrl = LinkUrlTB?.Text?.Replace(" ", "") ?? string.Empty;
-       if (string.IsNullOrEmpty(LinkUrl)){
-          MessageText.Text = "The string must be a valid URL and not contain any spaces.";
-          LinkUrlTB.Focus();
-          return;
-       }
        if (string.IsNullOrEmpty(LinkTitle)){
           MessageText.Text = "The string must be at least 1 character and not contain any spaces.";
           LinkTitleTB.Focus();
+          return;
+       }
+       if (string.IsNullOrEmpty(LinkUrl)){
+          MessageText.Text = "The string must be a valid URL and not contain any spaces.";
+          LinkUrlTB.Focus();
           return;
        }
        Close(true);
