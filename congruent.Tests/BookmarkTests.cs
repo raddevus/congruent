a8f31af1a21cd17b7768edc07ad25c97905385e3
raddevus <1radeutsch1@gmail.com>
2026-04-06 21:10:01 -0400

    Changed test, removed unneeded list of bookmarks - now using single bookmark obj to load from file which is correct

--- congruent.Tests/BookmarkTests.cs
+++ congruent.Tests/BookmarkTests.cs
@@ -8,14 +8,13 @@ public class BookmarkTests
     [Fact]
     async public Task ExamineBookmarksFromFile()
     {
-      List<Bookmark> allBookmarks = new();
-      // set bookmarkpath to the test debug directory
-      allBookmarks.Add(new Bookmark(){
+      Bookmark bm = new(){
            BookmarkPath=AppContext.BaseDirectory
-      });
-      Console.WriteLine($"base path => {allBookmarks[0].BookmarkPath}");
+      };
 
-      var bmList = await allBookmarks[0].LoadFromFile();
+      Console.WriteLine($"base path => {bm.BookmarkPath}");
+
+      var bmList = await bm.LoadFromFile();
       Console.WriteLine($"bm.Count {bmList.Count}");
     }
 }
