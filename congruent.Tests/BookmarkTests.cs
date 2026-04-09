cc1c32ba3e6edb98090a61789b61cb690e6ca42a
raddevus <1radeutsch1@gmail.com>
2026-04-08 23:04:44 -0400

    Added new test that just displays the Titles.  Nailed it! Every title of every link and folder shows up -- compared to ones that are loaded in the app.

--- congruent.Tests/BookmarkTests.cs
+++ congruent.Tests/BookmarkTests.cs
@@ -38,4 +38,32 @@ public class BookmarkTests
          }
       }
     }
+
+    [Fact]
+    async public Task ExamineBookmarksFromFileOutputTitles()
+    {
+      Bookmark bm = new(){
+           BookmarkPath=AppContext.BaseDirectory
+      };
+
+      Console.WriteLine($"base path => {bm.BookmarkPath}");
+
+      var bmList = await bm.LoadFromFile();
+      Console.WriteLine($"bm.Count {bmList.Count}");
+      List<Bookmark> allBms = new();
+      foreach (Bookmark b in bmList){
+         allBms.Add(b);
+      }
+      var targetCounter = allBms.Count;
+      var counter = 0;
+      for (int x = 0;x < allBms.Count; x++){
+         counter++;
+         Console.WriteLine($"b.Title : {allBms[x].Title}");
+            foreach (Bookmark i in allBms[x].Children){ allBms.Add(i);}
+         if (counter == targetCounter){
+            targetCounter = allBms[x].Children.Count;
+            counter = 0;
+         }
+      }
+    }
 }
