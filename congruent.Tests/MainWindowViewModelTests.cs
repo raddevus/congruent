470685c3696be6448eac038b6ffd87be61ff10f3
raddevus <1radeutsch1@gmail.com>
2026-04-13 16:58:34 -0400

    continuing to stub out the first test

--- congruent.Tests/MainWindowViewModelTests.cs
+++ congruent.Tests/MainWindowViewModelTests.cs
@@ -11,5 +11,18 @@ public class MainWindowViewModelTests
     async public Task FindBookmarkTest(){
        MainWindowViewModel vm = new();
        Console.WriteLine("I did it!");
+
+       Bookmark bm = new(){
+           BookmarkPath=AppContext.BaseDirectory
+      };
+
+      Console.WriteLine($"base path => {bm.BookmarkPath}");
+
+      var bmList = await bm.LoadFromFile();
+      foreach (Bookmark b in bmList){
+         vm.AllBookmarks.Add(b);
+      }
+      Console.WriteLine($"Test is ready. Have {vm.AllBookmarks.Count} bookmarks loaded.");
+
     }
 }
