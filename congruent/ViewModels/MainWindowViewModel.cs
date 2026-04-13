a689be820948c117d5c11f9e947f8b68ac946b0a
raddevus <1radeutsch1@gmail.com>
2026-04-13 16:41:30 -0400

    Added two new stub methods to ViewModel & removed unused Observablecollection

--- congruent/ViewModels/MainWindowViewModel.cs
+++ congruent/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections.ObjectModel;
 using congruent.Models;
+using System.Threading.Tasks;
 
 namespace congruent.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-   public ObservableCollection<WebLink> AllLinks{get;set;} = new();
    public ObservableCollection<Bookmark> AllBookmarks{get;set;} = new();
+
+   async public Task<Bookmark> FindTargetBookmark(){
+      return new Bookmark();
+   }
+
+   async public Task<Bookmark> FindBookmarkParent(){
+      return new Bookmark();
+   }
 
 }
