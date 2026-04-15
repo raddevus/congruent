a59287c5fbcb1de04cb29045f91b12bef045a512
raddevus <1radeutsch1@gmail.com>
2026-04-15 16:46:17 -0400

    Added method to VM to check for BM with duplicate link

--- congruent/ViewModels/MainWindowViewModel.cs
+++ congruent/ViewModels/MainWindowViewModel.cs
@@ -64,7 +64,8 @@ public partial class MainWindowViewModel : ViewModelBase
       //if Link is Null or Empty then we have a parent (folder)
       //otherwise we check to see if a normal bookmark exists
       var outMark = await FindTargetBookmark(inMark.Title, !string.IsNullOrEmpty(inMark.Link));
-      if (outMark == null){
+      var outByLink = await FindTargetBookmarkByLink(inMark.Link); 
+      if (outMark == null && outByLink == null){
          return false;
       }
       return true;
@@ -74,4 +75,51 @@ public partial class MainWindowViewModel : ViewModelBase
       return new Bookmark();
    }
 
+   async public Task<Bookmark> FindTargetBookmarkByLink(
+            string link,
+            //int hashcode,
+            bool isGetParent = false
+            ){
+
+            Console.WriteLine($"AllBookmarks.Count: {AllBookmarks.Count}");
+            Bookmark? targetBm = null;
+
+            List<Bookmark> allBms = new();
+            HashSet<Bookmark> allParents = new();
+            foreach (Bookmark b in AllBookmarks){
+               allBms.Add(b);
+            }
+
+            var targetCounter = allBms.Count;
+            var counter = 0;
+            Bookmark parent = null;
+            for (int x = 0;x < allBms.Count; x++){
+               counter++;
+               Console.WriteLine($"b.Title : {allBms[x].Title}");
+
+               //if (allBms[x].GetHashCode() == hashcode){
+               if (allBms[x].Link == link){
+                  if (isGetParent){
+                     if (allParents.Count >0){
+                     parent = allParents.ToList<Bookmark>()[allParents.Count-1];
+                     Console.WriteLine($"Found parent: {parent?.GetHashCode()} : {parent?.Title}");
+                     Console.WriteLine($"Found child: {allBms[x].GetHashCode()}");
+                     }
+                     return parent;
+                  }
+                  targetBm = allBms[x];
+                  return targetBm;
+               }
+               foreach (Bookmark i in allBms[x].Children){
+                  allParents.Add(allBms[x]);
+                 Console.WriteLine($"Parent title ==> {allBms[x].Title}");
+                  allBms.Add(i);}
+               if (counter == targetCounter){
+                  targetCounter = allBms[x].Children.Count;
+                  counter = 0;
+               }
+            }
+
+            return null;
+      }
 }
