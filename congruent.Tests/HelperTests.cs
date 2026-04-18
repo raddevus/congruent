de6970aa98bef2c989797b8437885d3caaee71fd
raddevus <1radeutsch1@gmail.com>
2026-04-18 18:25:12 -0400

    Added more urls to test CreateTabText

--- congruent.Tests/HelperTests.cs
+++ congruent.Tests/HelperTests.cs
@@ -10,5 +10,15 @@ public class HelperTests
     {
        string url = "https://newlibre.com/comicreader";
        Console.WriteLine($"url tab text: {url.CreateTabText()}");
+       url = "https://newlibre.com/";
+       Console.WriteLine($"url tab text: {url.CreateTabText()}");
+       url = "http://rpix.local";
+       Console.WriteLine($"url tab text: {url.CreateTabText()}");
+       url = "https://newlibre.com/maze";
+       Console.WriteLine($"url tab text: {url.CreateTabText()}");
+       url = "https://computerworld.com";
+       Console.WriteLine($"url tab text: {url.CreateTabText()}");
+       url = "https://archive.org";
+       Console.WriteLine($"url tab text: {url.CreateTabText()}");
     }
 }
