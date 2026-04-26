7504c75217742c6647b828f86e3f5a5d851e6980
raddevus <1radeutsch1@gmail.com>
2026-04-26 16:55:37 -0400

    Added FindTargetByHashCode - which will be the definitive way to find a specific bookmark.

--- congruent/ViewModels/MainWindowViewModel.cs
+++ congruent/ViewModels/MainWindowViewModel.cs
@@ -78,7 +78,54 @@ public partial class MainWindowViewModel : ViewModelBase
       return new Bookmark();
    }
 
-   async public Task<Bookmark> FindTargetBookmarkByLink(
+   async public Task<Bookmark> FindTargetBookmarkByHashCode(int hashcode,
+            int? phash =null){
+
+            Console.WriteLine($"AllBookmarks.Count: {AllBookmarks.Count}");
+            Bookmark? targetBm = null;
+
+            List<Bookmark> allBms = new();
+            HashSet<Bookmark> allParents = new();
+            foreach (Bookmark b in AllBookmarks){
+               allBms.Add(b);
+            }
+
+            var targetCounter = allBms.Count;
+            var counter = 0;
+            Bookmark parent = null;
+            for (int x = 0;x < allBms.Count; x++){
+               counter++;
+               Console.WriteLine($"b.Title : {allBms[x].Title}");
+
+               //if (allBms[x].GetHashCode() == hashcode){
+               if (allBms[x].GetHashCode() == hashcode){
+                  if (phash != null){
+                     foreach (Bookmark b in allParents){
+                        if (b.GetHashCode() == hashcode){
+                           parent = b;
+                        }
+                     }
+                     Console.WriteLine($"Found parent: {parent?.GetHashCode()} : {parent?.Title}");
+                     Console.WriteLine($"Found child: {allBms[x].GetHashCode()}");
+                     return parent;
+                  }
+                  targetBm = allBms[x];
+                  return targetBm;
+               }
+               foreach (Bookmark i in allBms[x].Children){
+                  allParents.Add(allBms[x]);
+                 Console.WriteLine($"Parent title ==> {allBms[x].Title}");
+                  allBms.Add(i);
+               }
+               if (counter == targetCounter){
+                  targetCounter = allBms[x].Children.Count;
+                  counter = 0;
+               }
+            }
+            return null;
+      }
+   
+      async public Task<Bookmark> FindTargetBookmarkByLink(
             string link,
             //int hashcode,
             bool isGetParent = false
