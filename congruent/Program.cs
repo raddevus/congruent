b9cbba7df972ca122617f6ae0b45abec5771e040
raddevus <1radeutsch1@gmail.com>
2026-08-16 12:13:08 -0400

    removed code that was attempting to disable gpu extras to fix wayland issue.

--- congruent/Program.cs
+++ congruent/Program.cs
@@ -9,12 +9,8 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) {
-       // Pass flags to the Chromium embedded browser process
-    Environment.SetEnvironmentVariable("CEF_SWITCHES", "--disable-gpu --disable-gpu-compositing");
-        BuildAvaloniaApp()
+    public static void Main(string[] args) => BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
-    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
